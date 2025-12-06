c21289df4de3d7dda6c7d49b464f01038ce020a2
Geremia Francesco Conca <geremia.conca@studio.unibo.it>
2025-12-06 18:42:26 +0100

    implemented the class (raw code)

--- 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 112-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -1,7 +1,15 @@
 package it.unibo.oop.reactivegui02;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import it.unibo.oop.JFrameUtil;
+
 import java.io.Serial;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Second example of reactive GUI.
@@ -10,4 +18,103 @@ public final class ConcurrentGUI extends JFrame {
 
     @Serial
     private static final long serialVersionUID = 1L;
+    private final JLabel display = new JLabel();
+
+    public ConcurrentGUI() {
+        super();
+        JFrameUtil.dimensionJFrame(this);
+        final JPanel canvas = new JPanel();
+        this.add(canvas);
+        final JButton stop = new JButton("stop");
+        final JButton up = new JButton("up");
+        final JButton down = new JButton("down");
+        canvas.add(display);
+        canvas.add(stop);
+        canvas.add(up);
+        canvas.add(down);
+        this.setVisible(true);
+        final Agent agent = new Agent();
+        up.addActionListener(e -> agent.countUp());
+        down.addActionListener(e -> agent.countDown());
+        stop.addActionListener(e -> agent.stopCounting());
+        new Thread(agent).start();
+    }
+
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private final class Agent implements Runnable {
+        /*
+         * Stop is volatile to ensure visibility. Look at:
+         *
+         * http://archive.is/9PU5N - Sections 17.3 and 17.4
+         *
+         * For more details on how to use volatile:
+         *
+         * http://archive.is/4lsKW
+         *
+         */
+        private volatile boolean stop;
+        private volatile boolean positive = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!stop){
+                if(positive){
+                    positiveCount();
+                } else {
+                    negativeCount();
+                }
+            }
+        }
+
+        private void negativeCount() {
+            while (!this.stop && !this.positive) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                    this.counter--;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+
+        private void positiveCount() {
+            while (!this.stop && this.positive) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextText));
+                    this.counter++;
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        /**
+         * External command to change counting mode.
+         */
+        public void countUp() {
+            this.positive = true;
+        }
+
+        public void countDown() {
+            this.positive = false;
+        }
+    }
 }
