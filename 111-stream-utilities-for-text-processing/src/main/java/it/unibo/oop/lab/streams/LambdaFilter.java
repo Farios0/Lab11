86a7d2547d91b70094b3191e64e7a945f47623fa
Geremia Francesco Conca <geremia.conca@studio.unibo.it>
2025-12-07 19:45:57 +0100

    implemented the class throught the exercise guidelines

--- 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 111-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -7,6 +7,9 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.io.Serial;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -41,7 +44,23 @@ public final class LambdaFilter extends JFrame {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TO_LOWER_CASE("Lowercase", String::toLowerCase),
+        CHAR_NUMBER("count char number", s -> String.valueOf(s.chars().count())),
+        LINES_NUMBER("count lines number", s -> String.valueOf(s.lines().count())),
+        WORD_COUNTER("return a number per word", s -> {
+            String[] words = (s.split(" "));
+            for (int i = 0; i < words.length; i++) {
+                words[i] = words[i].concat(" -> " + i);    
+            }
+            return Arrays.toString(words);
+        }),
+        SORT_WORDS("sort word in alphabetic orders", s -> {
+            String[] result = (s.split(" "));
+            Arrays.sort(result);
+            return Arrays.toString(result);
+        });
+        
 
         private final String commandName;
         private final Function<String, String> fun;
